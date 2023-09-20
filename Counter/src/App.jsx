d7245ee722849766262f77dbc87ed7846694f989
Yogeshwar B <byogeshwarreddy@gmail.com>
2023-09-20 23:41:02 +0530

    Added Counter

---
+++ Counter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+
+const App=()=>{
+const [count,ChangeCounter]=useState(0);
+  return (<center>
+    <button onClick={()=>ChangeCounter((oldCount)=>oldCount+1)}>
+      +
+    </button>
+    <div>
+    {count}
+  </div>
+  <button onClick={()=>ChangeCounter((oldCount)=>oldCount-1)}>
+    -
+  </button>
+  </center>)
+};
+
+
+
+export default App;
