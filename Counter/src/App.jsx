94b83eda2cbb51b236e68d0c52149189d66a3420
yogeshwar-b <byogeshwarreddy@gmail.com>
2023-09-22 12:05:12 +0530

    Formatting changes

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 
-
-const App=()=>{
-const [count,ChangeCounter]=useState(0);
-  return (<center>
-    <button onClick={()=>ChangeCounter((oldCount)=>oldCount+1)}>
-      +
-    </button>
-    <div>
-    {count}
-  </div>
-  <button onClick={()=>ChangeCounter((oldCount)=>oldCount-1)}>
-    -
-  </button>
-  </center>)
+const App = () => {
+  const [count, ChangeCounter] = useState(0);
+  return (
+    <center>
+      <button onClick={() => ChangeCounter((oldCount) => oldCount + 1)}>
+        +
+      </button>
+      <div>{count}</div>
+      <button onClick={() => ChangeCounter((oldCount) => oldCount - 1)}>
+        -
+      </button>
+    </center>
+  );
 };
-
-
 
 export default App;
