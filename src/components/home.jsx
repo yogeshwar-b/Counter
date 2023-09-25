88a25f4e422382b64a93a23e312a3534664314d0
Yogeshwar B <byogeshwarreddy@gmail.com>
2023-09-26 00:26:15 +0530

    Added fade transition.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import "./home.css";
+import { CSSTransition } from "react-transition-group";
 
 const Counter = () => {
   const [count, ChangeCounter] = useState(0);
+  const [isEnter, ChangeIsEnter] = useState(true);
+  function timeout(delay) {
+    return new Promise((res) => setTimeout(res, delay));
+  }
 
   return (
     <div className="grid-container">
       <div
         className="fsize grid-item"
-        onClick={() => ChangeCounter((oldCount) => oldCount - 1)}
+        onClick={async () => {
+          ChangeIsEnter(false);
+          await timeout(500);
+          ChangeCounter((oldCount) => oldCount - 1);
+          ChangeIsEnter(true);
+        }}
       >
         -
       </div>
-      <div className="fsize grid-item">{count}</div>
+      <CSSTransition in={isEnter} timeout={500} classNames="animate-count">
+        <div className="fsize grid-item">{count}</div>
+      </CSSTransition>
       <div
         className="fsize grid-item"
-        onClick={() => ChangeCounter((oldCount) => oldCount + 1)}
+        onClick={async () => {
+          if (isEnter) ChangeIsEnter(false);
+          await timeout(500);
+          ChangeCounter((oldCount) => oldCount + 1);
+          ChangeIsEnter(true);
+        }}
       >
         +
       </div>
