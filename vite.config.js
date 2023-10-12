3d78ce847df552b0d9fef12cc90a9dffd9031619
Yogeshwar B <40350972+yogeshwar-b@users.noreply.github.com>
2023-10-12 17:29:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://yogeshwar-b.github.io/Counter/",
+  base: "/Counter/",
   plugins: [react()],
 });
