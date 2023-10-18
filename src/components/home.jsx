0c3aefa1c2286d4a6d66ada2776b80d01c5382c9
yogeshwar-b <byogeshwarreddy@gmail.com>
2023-10-18 16:23:15 +0530

    Transitions modification and Fixed the 9-10 bug where centering was not working

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,6 +3,8 @@ import "./home.css";
 import { CSSTransition, Transition } from "react-transition-group";
 import { TransitionTimeOut } from "./constants";
 import { ThemeContext, TransitionContext } from "../App";
+import { TransitionList } from "./constants";
+
 import { red } from "@mui/material/colors";
 function timeout(delay) {
   return new Promise((res) => setTimeout(res, delay));
@@ -12,25 +14,31 @@ var isProcessing = false;
 const Counter = () => {
   const themeclass = useContext(ThemeContext);
   const { transitionName, setTransitionName } = useContext(TransitionContext);
-  const [count, ChangeCounter] = useState(0);
+  const [count1, ChangeCounter1] = useState(0);
+  const [count2, ChangeCounter2] = useState(0);
   //Determines if the state is entering or exiting.
   const [isEnter1, ChangeIsEnter1] = useState(true);
   const [isEnter2, ChangeIsEnter2] = useState(false);
   // const textsizeclass = "fsize" + themeclass;
   const duration = 400;
 
-  const defaultStyle = {
-    // position: "absolute",
-    // transition: `opacity ${duration}ms`,
-    opacity: 0,
-  };
+  // const defaultStyle = {
+  //   // position: "absolute",
+  //   // transition: `opacity ${duration}ms`,
+  //   opacity: 0,
+  // };
 
-  const transitionStyles = {
+  const transitionFade = {
     entering: { opacity: 1, transition: `opacity ${duration}ms ease-in` },
     entered: { opacity: 1 },
     exiting: { opacity: 0, transition: `opacity ${duration}ms ease-out` },
     exited: { opacity: 0 },
   };
+
+  // const transi = TransitionList.filter((obj) => {
+  //   return obj.name == transitionName;
+  // });
+  // console.log(transitionName, TransitionList, transi);
 
   return (
     <div className="grid-container">
@@ -41,13 +49,13 @@ const Counter = () => {
             isProcessing = true;
             if (isEnter1) {
               isEnter1 ? ChangeIsEnter1(false) : ChangeIsEnter1(true);
-              await timeout(duration);
-              ChangeCounter((oldCount) => oldCount - 1);
+              // await timeout(duration);
+              ChangeCounter2(count1 - 1);
               isEnter2 ? ChangeIsEnter2(false) : ChangeIsEnter2(true);
             } else {
               isEnter2 ? ChangeIsEnter2(false) : ChangeIsEnter2(true);
-              await timeout(duration);
-              ChangeCounter((oldCount) => oldCount - 1);
+              // await timeout(duration);
+              ChangeCounter1(count2 - 1);
               isEnter1 ? ChangeIsEnter1(false) : ChangeIsEnter1(true);
             }
             isProcessing = false;
@@ -56,18 +64,18 @@ const Counter = () => {
       >
         -
       </div>
-      <div className="grid-item">
+      <div className="grid-item flex-center">
         <Transition in={isEnter1} timeout={duration}>
           {(state) => (
             <div
               style={{
-                ...defaultStyle,
-                ...transitionStyles[state],
+                // ...transi[state],
+                ...transitionFade[state],
                 position: "absolute",
               }}
               className={"fsize" + themeclass}
             >
-              {count}
+              {count1}
             </div>
           )}
         </Transition>
@@ -75,12 +83,13 @@ const Counter = () => {
           {(state) => (
             <div
               style={{
-                ...defaultStyle,
-                ...transitionStyles[state],
+                // ...transi[state],
+                ...transitionFade[state],
+                position: "absolute",
               }}
               className={"fsize" + themeclass}
             >
-              {count}
+              {count2}
             </div>
           )}
         </Transition>
@@ -90,13 +99,13 @@ const Counter = () => {
         onClick={async () => {
           if (isEnter1) {
             isEnter1 ? ChangeIsEnter1(false) : ChangeIsEnter1(true);
-            await timeout(duration);
-            ChangeCounter((oldCount) => oldCount + 1);
+            // await timeout(duration);
+            ChangeCounter2(count1 + 1);
             isEnter2 ? ChangeIsEnter2(false) : ChangeIsEnter2(true);
           } else {
             isEnter2 ? ChangeIsEnter2(false) : ChangeIsEnter2(true);
-            await timeout(duration);
-            ChangeCounter((oldCount) => oldCount + 1);
+            // await timeout(duration);
+            ChangeCounter1(count2 + 1);
             isEnter1 ? ChangeIsEnter1(false) : ChangeIsEnter1(true);
           }
         }}
