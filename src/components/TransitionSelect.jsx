346bb031634fe0a22dc90848dd39777ce87de8c1
Yogeshwar B <byogeshwarreddy@gmail.com>
2023-10-19 01:02:25 +0530

    Added Transtions to constants.js

--- src/components/TransitionSelect.jsx
+++ src/components/TransitionSelect.jsx
@@ -1,32 +1,32 @@
-import PropTypes from "prop-types";
 import { TransitionList } from "./constants";
 import "./TransitionSelect.css";
 import { useContext } from "react";
 import { TransitionContext } from "../App";
 export const TransitionSelect = () => {
-  const { TransitionName, setTransitionName } = useContext(TransitionContext);
+  const { transitionName, setTransitionName } = useContext(TransitionContext);
   return (
     <div>
       <select
         className="flat-select"
         name="TransitionSelect"
         id="TransitionSelect"
-        value={TransitionName}
+        value={transitionName}
         onChange={(e) => {
           setTransitionName(e.target.value);
         }}
       >
         {TransitionList.map((item) => {
           return (
-            <option key={item} className="flat-select-option" value={item}>
-              {item}
+            <option
+              key={item.name}
+              className="flat-select-option"
+              value={item.name}
+            >
+              {item.name}
             </option>
           );
         })}
       </select>
     </div>
   );
-};
-TransitionSelect.propTypes = {
-  label: PropTypes.string,
 };
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,33 @@
 // Transition Time out in milliseconds
-export const TransitionTimeOut = 2000;
+export const TransitionDuration = 400;
 // List of Transitions for Transition Select list
-export const TransitionList = ["none", "fade"];
+export const TransitionList = [
+  {
+    name: "None",
+    transitiondata: {
+      entering: {
+        opacity: 1,
+      },
+      entered: { opacity: 1 },
+      exiting: {
+        opacity: 0,
+      },
+      exited: { opacity: 0 },
+    },
+  },
+  {
+    name: "Fade",
+    transitiondata: {
+      entering: {
+        opacity: 1,
+        transition: `opacity ${TransitionDuration}ms ease-in`,
+      },
+      entered: { opacity: 1 },
+      exiting: {
+        opacity: 0,
+        transition: `opacity ${TransitionDuration}ms ease-out`,
+      },
+      exited: { opacity: 0 },
+    },
+  },
+];
