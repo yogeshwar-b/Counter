843ae896621ca74ef42c64c214682d04fd38de29
Yogeshwar B <byogeshwarreddy@gmail.com>
2023-10-19 01:02:25 +0530

    Added Transtions to constants.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { TransitionSelect } from "./components/TransitionSelect";
 import { createContext, useState } from "react";
 import "./App.css";
 import MaterialUISwitch from "./components/switch";
+import { TransitionList } from "./components/constants";
 export const ThemeContext = createContext();
 export const TransitionContext = createContext();
 
 const App = () => {
-  const [transitionName, setTransitionName] = useState("none");
+  const [transitionItem, setTransitionName] = useState(TransitionList[0].name);
   const [themeclass, setThemeClass] = useState("-dark");
   return (
-    <TransitionContext.Provider value={{ transitionName, setTransitionName }}>
+    <TransitionContext.Provider value={{ transitionItem, setTransitionName }}>
       <ThemeContext.Provider value={themeclass}>
         <div className={"mainplate " + "mainplate" + themeclass}>
           <div className="div-center">
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,44 +1,27 @@
 import { useContext, useState } from "react";
 import "./home.css";
-import { CSSTransition, Transition } from "react-transition-group";
-import { TransitionTimeOut } from "./constants";
+import { Transition } from "react-transition-group";
+import { TransitionDuration } from "./constants";
 import { ThemeContext, TransitionContext } from "../App";
 import { TransitionList } from "./constants";
 
-import { red } from "@mui/material/colors";
-function timeout(delay) {
-  return new Promise((res) => setTimeout(res, delay));
-}
+// function timeout(delay) {
+//   return new Promise((res) => setTimeout(res, delay));
+// }
 var isProcessing = false;
 
 const Counter = () => {
   const themeclass = useContext(ThemeContext);
-  const { transitionName, setTransitionName } = useContext(TransitionContext);
+  const { transitionItem, setTransitionName } = useContext(TransitionContext);
   const [count1, ChangeCounter1] = useState(0);
   const [count2, ChangeCounter2] = useState(0);
   //Determines if the state is entering or exiting.
   const [isEnter1, ChangeIsEnter1] = useState(true);
   const [isEnter2, ChangeIsEnter2] = useState(false);
-  // const textsizeclass = "fsize" + themeclass;
-  const duration = 400;
-
-  // const defaultStyle = {
-  //   // position: "absolute",
-  //   // transition: `opacity ${duration}ms`,
-  //   opacity: 0,
-  // };
-
-  const transitionFade = {
-    entering: { opacity: 1, transition: `opacity ${duration}ms ease-in` },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0, transition: `opacity ${duration}ms ease-out` },
-    exited: { opacity: 0 },
-  };
-
-  // const transi = TransitionList.filter((obj) => {
-  //   return obj.name == transitionName;
-  // });
-  // console.log(transitionName, TransitionList, transi);
+  // console.log(transitionItem);
+  const currentTransition = TransitionList.find((obj) => {
+    return obj.name == transitionItem;
+  }).transitiondata;
 
   return (
     <div className="grid-container">
@@ -65,12 +48,11 @@ const Counter = () => {
         -
       </div>
       <div className="grid-item flex-center">
-        <Transition in={isEnter1} timeout={duration}>
+        <Transition in={isEnter1} timeout={TransitionDuration}>
           {(state) => (
             <div
               style={{
-                // ...transi[state],
-                ...transitionFade[state],
+                ...currentTransition[state],
                 position: "absolute",
               }}
               className={"fsize" + themeclass}
@@ -79,12 +61,11 @@ const Counter = () => {
             </div>
           )}
         </Transition>
-        <Transition in={isEnter2} timeout={duration}>
+        <Transition in={isEnter2} timeout={TransitionDuration}>
           {(state) => (
             <div
               style={{
-                // ...transi[state],
-                ...transitionFade[state],
+                ...currentTransition[state],
                 position: "absolute",
               }}
               className={"fsize" + themeclass}
