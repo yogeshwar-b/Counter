c4cb8a98d07134899edd7496e6ddeba47cdba1cb
Yogeshwar B. <40350972+yogeshwar-b@users.noreply.github.com>
2025-07-01 23:07:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  base: "/Counter/",
-  plugins: [react()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [vercel()],
 });
